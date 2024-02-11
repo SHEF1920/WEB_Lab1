f09a195ff05cd42dfbc3e3608992c26106badfeb
Nickolay Shishelyakin <shne2003@gmail.com>
2024-02-11 16:54:44 +0300

    LAB 4!

--- time_script.js
+++ time_script.js
@@ -1,6 +1,3 @@
-//IIFE(Immediately Invoked Function Expression) - функция, которая вызывается немедленно после ее создания.
-// Такой подход часто используется в JS для создания локальной области видомости, предотвращения утечек переменных в глобальную область видимости и изолиции кода
-
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('load', function() {
         var loadTime = performance.now();
@@ -8,24 +5,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var element = document.getElementById('loadTimeInfo');
         element.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(3) + ' мс';
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var url = window.location.pathname;
-    var filename = url.substring(url.lastIndexOf('/') + 1);
-
-    // получение всех элементов меню
-    var menuItems = document.querySelectorAll('.menu-list li');
-
-    // Перебираем элементы и добавляем класс active к текущей странице
-    menuItems.forEach(function (item) {
-        var link = item.querySelector('a');
-        var href = link.getAttribute('href');
-
-        if (filename == href) {
-            item.classList.add('active');
-        }
     });
 });
