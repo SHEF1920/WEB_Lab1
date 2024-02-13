4786c1e3613bb697d149c6d5b366666a5d937b59
Nickolay Shishelyakin <shne2003@gmail.com>
2024-02-13 18:29:31 +0300

    FIX LAB4!

--- time_script.js
+++ time_script.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
         element.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(3) + ' мс';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var url = window.location.pathname;
+    var filename = url.substring(url.lastIndexOf('/') + 1);
+
+    var menuItems = document.querySelectorAll('.navbar li');
+
+    menuItems.forEach(function (item) {
+        var link = item.querySelector('a');
+        var href = link.getAttribute('href');
+
+        if (filename == href) {
+            item.classList.add('active');
+        }
+    });
+});
